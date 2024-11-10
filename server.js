2df2922bf1be47084c38013080d31ef98f06355e
DreddzErp <dredderp@gmail.com>
2024-11-10 22:40:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ function isValidPassword(password) {
 // Login Rate Limiter
 const loginLimiter = rateLimit({
   windowMs: 30 * 60 * 1000, // 30 minutes
-  max: 5, // Limit each IP to 5 requests per windowMs
+  max: 10, // Limit each IP to 5 requests per windowMs
   message: 'Too many login attempts, please try again after 30 minutes.',
   handler: function (req, res, next, options) {
     res.status(options.statusCode).json({ success: false, message: options.message });
